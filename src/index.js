086ca41e3442a5170bc9f5581594e2bba4f2e299
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-10 08:31:36 +0300

    added and configured react-router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 import './index.css';
 import App from './App';
+import Err404 from './components/Err404';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Router history={hashHistory}>
+  <Route path="/" component={App} />
+  <Route path="*" component={Err404} />
+</Router>,
+document.getElementById('root'));
 registerServiceWorker();
