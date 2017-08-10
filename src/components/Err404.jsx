f8552bc020a652335080959f3b34da41318d1bf7
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-10 17:39:19 +0300

    introduced bootstrap styles in main html file

--- src/components/Err404.jsx
+++ src/components/Err404.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-
 const Err404 = () => (
   <div>
-    <h1>Err, route not found</h1>
+    <h1>Error, route not found</h1>
     <p>Back to <a href="/">Home</a></p>
-  </div>)
+  </div>);
 
 export default Err404;
