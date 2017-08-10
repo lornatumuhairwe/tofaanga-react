847d2138f9ea9361731086bd33dd43a646e3668e
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-10 17:45:59 +0300

    setup component to handle non existent routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import LoginForm from './components/Login';
 import './App.css';
 
--- src/components/Err404.jsx
+++ src/components/Err404.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
-const Err404 = () => (
-  <div>
-    <h1>Error, route not found</h1>
-    <p>Back to <a href="/">Home</a></p>
-  </div>);
+import React, { Component } from 'react';
+class Err404 extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Error, route not found</h1>
+                <p>Back to <a href="/">Home</a></p>
+            </div>);
+
+    }
+}
 
 export default Err404;
