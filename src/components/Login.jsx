03618c47a16aa6eb9bcb0f46a6541b656a8e1015
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-17 18:55:03 +0300

    added isLoggedIn state in Bucketlist component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,17 +7,17 @@ import Bucketlist from './bucketlist';
 class LoginForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { email: '', pwd: '' };
+    this.state = { email: '', pwd: '', isLoggedIn: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.baseURL = 'https://tofaangapi.herokuapp.com/auth/login';
     this.displaySignupForm = this.displaySignupForm.bind(this);
     this.token = '';
   }
-    // getToken() {
-    //     this.props.getToken(this.token);
-    //     this.props.displaySignup(false);
-    // }
+  // getToken() {
+  //     this.props.getToken(this.token);
+  //     this.props.displaySignup(false);
+  // }
 
   displaySignupForm() {
     this.props.displaySignup(true);
@@ -30,6 +30,7 @@ class LoginForm extends Component {
     LoginFormData.append('password', this.state.pwd);
     this.sendLogin(this.baseURL, LoginFormData);
     this.setState({ email: '', pwd: '' });
+    console.log(this.props);
     this.props.getToken(this.token);
     // console.log(this.props, this)
     // this.props.displaySignup(false);
@@ -40,6 +41,7 @@ class LoginForm extends Component {
     this.setState(newState);
   }
   sendLogin(url, data) {
+    console.log('Im sending the data');
     const postData = {
       method: 'POST',
       body: data,
@@ -49,7 +51,12 @@ class LoginForm extends Component {
       .then((resjson) => {
         console.log(this);
         if (resjson.status === 'success') {
-          this.token = resjson.auth_token;
+          console.log(resjson.auth_token);
+          if (resjson.auth_token.length > 0) {
+            this.token = resjson.auth_token;
+            console.log(this.token);
+            this.setState({ isLoggedIn: true });
+          }
         } else if (resjson.message === 'Password mismatch') {
           window.location = '/auth/login';
         } else {
@@ -59,49 +66,54 @@ class LoginForm extends Component {
   }
 
   render() {
-    return (
-      <div>
-        <ul className="navigation">
-          <button id="si" onClick={this.displaySignupForm}>Sign up</button>
-          <li><a href="#"><strong>TOFAANGA</strong></a></li>
-        </ul>
-        <div className="signupbackground">
-          <div className="overlay">
-            <div className="signinbox">
-              <div className="title"><strong>Sign in</strong></div>
-              <form onSubmit={this.handleSubmit}>
-                <div className="input-group">
-                  <span className="input-group-addon"><i className="fa fa-at" /></span>
-                  <input
-                    type="email"
-                    className="form-control"
-                    id="email"
-                    value={this.state.email}
-                    placeholder="email"
-                    required
-                    onChange={this.handleChange.bind(this, 'email')}
-                  />
-                </div>
-                <div className="input-group">
-                  <span className="input-group-addon"><i className="fa fa-lock" /></span>
-                  <input
-                    type="password"
-                    className="form-control"
-                    id="pwd"
-                    value={this.state.pwd}
-                    required
-                    onChange={this.handleChange.bind(this, 'pwd')}
-                  />
-                </div>
-                <button type="submit" className="btn btn-default">Sign in</button>
-                <div>
-                  <a href="#">Forgot Password?</a>
-                </div>
-              </form>
+    if (!this.state.isLoggedIn) {
+      return (
+        <div>
+          <ul className="navigation">
+            <button id="si" onClick={this.displaySignupForm}>Sign up</button>
+            <li><a href="#"><strong>TOFAANGA</strong></a></li>
+          </ul>
+          <div className="signupbackground">
+            <div className="overlay">
+              <div className="signinbox">
+                <div className="title"><strong>Sign in</strong></div>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="input-group">
+                    <span className="input-group-addon"><i className="fa fa-at" /></span>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="email"
+                      value={this.state.email}
+                      placeholder="email"
+                      required
+                      onChange={this.handleChange.bind(this, 'email')}
+                    />
+                  </div>
+                  <div className="input-group">
+                    <span className="input-group-addon"><i className="fa fa-lock" /></span>
+                    <input
+                      type="password"
+                      className="form-control"
+                      id="pwd"
+                      value={this.state.pwd}
+                      required
+                      onChange={this.handleChange.bind(this, 'pwd')}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-default">Sign in</button>
+                  <div>
+                    <a href="#">Forgot Password?</a>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      );
+    }
+    return (
+      <Bucketlist token={this.state.token} />
     );
   }
 }
