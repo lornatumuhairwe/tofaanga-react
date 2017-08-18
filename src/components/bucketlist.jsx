288394cd60fd63d20c00e972be200f7b5563e2d9
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-19 00:20:35 +0300

    changed Bucketlist items from table to ul

--- src/components/bucketlist.jsx
+++ src/components/bucketlist.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import '../styles/css/bootstrap.min.css';
-import '../styles/css/style.css';
+import '../styles/css/bucketlist.css';
 
 class BLRow extends React.Component {
   render() {
     return (
-      <tr>
-        <td>{this.props.bucketlist}</td>
-      </tr>
+      <li className="list-group-item">
+        <a href="#">{this.props.bucketlist}</a>
+        <div className="pull-right">
+          <form className="form-inline">
+            <input type="hidden" name="name" value={this.props.id} required />
+            <button type="submit" className="btn btn-xs">
+              <i className="glyphicon glyphicon-trash" /></button>
+            <button type="submit" className="btn btn-xs">
+              <i className="glyphicon glyphicon-edit" /></button>
+          </form>
+        </div>
+      </li>
     );
   }
 }
@@ -18,18 +27,13 @@ class BucketlistTable extends React.Component {
     const bls = this.props.bucketlists;
     for (const key in bls) {
       if (bls.hasOwnProperty(key)) {
-        rows.push(<BLRow bucketlist={bls[key]} />);
+        rows.push(<BLRow id={key} bucketlist={bls[key]} />);
       }
     }
     return (<div>
-      <table>
-        <thead>
-          <tr><b>Bucketlists</b></tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
+      <ul className="list-group col-md-3">
+        {rows}
+      </ul>
     </div>
     );
   }
@@ -57,6 +61,16 @@ class Bucketlist extends Component {
 
   render() {
     return (<div>
+      <nav className="navbar navbar-inverse">
+        <ul className="nav navbar-nav">
+          <li><a href="#">TOFAANGA</a></li>
+        </ul>
+        <ul className="nav navbar-nav navbar-right">
+          <li ><a href="#">Logout</a></li>
+        </ul>
+      </nav>
+      <button type="submit" className="btn btn-xs">
+        <i className="glyphicon glyphicon-trash" /></button>
       <BucketlistTable bucketlists={this.state.bucketlists} />
     </div>
     );
