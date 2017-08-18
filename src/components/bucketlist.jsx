540d76f4bca9008678c20771d87db207ecea1f3c
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-18 17:28:03 +0300

    cleaned up code

--- src/components/bucketlist.jsx
+++ src/components/bucketlist.jsx
@@ -3,37 +3,34 @@ import '../styles/css/bootstrap.min.css';
 import '../styles/css/style.css';
 
 class BLRow extends React.Component {
-    render() {
-        return (
-            <tr>
-              <td>{this.props.bucketlist}</td>
-            </tr>
-        );
-    }
+  render() {
+    return (
+      <tr>
+        <td>{this.props.bucketlist}</td>
+      </tr>
+    );
+  }
 }
 
 class BucketlistTable extends React.Component {
   render() {
-    let rows = [];
-    console.log('Your bucketlists are: ');
-    let bls = this.props.bucketlists;
-    for (let key in bls) {
-      console.log('For loop: ');
+    const rows = [];
+    const bls = this.props.bucketlists;
+    for (const key in bls) {
       if (bls.hasOwnProperty(key)) {
-        console.log(key + ' -> ' + bls[key]);
-        rows.push(<BLRow bucketlist={bls[key]} />)
+        rows.push(<BLRow bucketlist={bls[key]} />);
       }
     }
     return (<div>
-          <table>
-            <thead>
-            <tr><b>Bucketlists</b></tr>
-            </thead>
-            <tbody>
-            {rows}
-            </tbody>
-          </table>
-        </div>
+      <table>
+        <thead>
+          <tr><b>Bucketlists</b></tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>
+      </table>
+    </div>
     );
   }
 }
@@ -51,9 +48,7 @@ class Bucketlist extends Component {
       headers: { Authorization: this.props.token },
     };
     return fetch(url, getData)
-      .then(response => response.json(),
-        // this.setState({ bucketlists: response.json() });
-      ).then(res => this.setState({ bucketlists: res }));
+      .then(response => response.json()).then(res => this.setState({ bucketlists: res }));
   }
 
   componentDidMount() {
@@ -62,7 +57,7 @@ class Bucketlist extends Component {
 
   render() {
     return (<div>
-          <BucketlistTable bucketlists={this.state.bucketlists} />
+      <BucketlistTable bucketlists={this.state.bucketlists} />
     </div>
     );
   }
