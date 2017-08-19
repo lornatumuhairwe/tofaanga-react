3776c7e95b7d3f7effd00fef3d59ea77ab4b14c8
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-19 18:59:56 +0300

    features to view and add bucketlists

--- src/components/bucketlist.jsx
+++ src/components/bucketlist.jsx
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
 import '../styles/css/bootstrap.min.css';
 import '../styles/css/bucketlist.css';
 
 class BLRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.deleteBucketlist = this.deleteBucketlist.bind(this);
+    this.editBucketlist = this.editBucketlist.bind(this);
+    this.state = { bucketlist: '' };
+  }
+  deleteBucketlist(e) {
+    e.preventDefault();
+    const bID = this.refs.bid.value;
+    alert(`Do you really want to delete this item? : ${bID}`);
+  }
+  editBucketlist(e) {
+    e.preventDefault();
+    const bID = this.refs.bid.value;
+    alert(`Editing: ${bID}`);
+  }
+
   render() {
     return (
       <li className="list-group-item">
         <a href="#">{this.props.bucketlist}</a>
         <div className="pull-right">
           <form className="form-inline">
-            <input type="hidden" name="name" value={this.props.id} required />
-            <button type="submit" className="btn btn-xs">
-              <i className="glyphicon glyphicon-trash" /></button>
-            <button type="submit" className="btn btn-xs">
+            <input
+              type="hidden"
+              name="name"
+              value={this.props.id}
+              ref="bid"
+              required
+            />
+            <button type="submit" className="btn btn-xs" onClick={this.editBucketlist}>
               <i className="glyphicon glyphicon-edit" /></button>
+            <button type="submit" className="btn btn-xs" onClick={this.deleteBucketlist}>
+              <i className="glyphicon glyphicon-trash" /></button>
           </form>
         </div>
       </li>
@@ -42,8 +66,13 @@ class BucketlistTable extends React.Component {
 class Bucketlist extends Component {
   constructor(props) {
     super(props);
-    this.state = { bucketlists: '' };
+    this.state = { bucketlists: '', showModal: false, bname: '' };
     this.baseURL = 'https://tofaangapi.herokuapp.com/bucketlists/';
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.addBucketlist = this.addBucketlist.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
+    this.getName = this.getName.bind(this);
   }
 
   getBucketlists(url) {
@@ -53,6 +82,41 @@ class Bucketlist extends Component {
     };
     return fetch(url, getData)
       .then(response => response.json()).then(res => this.setState({ bucketlists: res }));
+  }
+
+  closeModal() {
+    this.setState({ showModal: false });
+  }
+
+  openModal() {
+    this.setState({ showModal: true });
+  }
+  getName(field, event) {
+    const newState = {};
+    newState[field] = event.target.value;
+    this.setState(newState);
+  }
+  addBucketlist(url, data) {
+    const postData = {
+      method: 'POST',
+      body: data,
+      headers: { Authorization: this.props.token },
+    };
+    return fetch(url, postData)
+      .then(response => response.json())
+      .then((resjson) => {
+        if (resjson.bucketlist) {
+          console.log(resjson.bucketlist);
+          this.getBucketlists(this.baseURL);
+        }
+      });
+  }
+  handleAdd(e) {
+    e.preventDefault();
+    const BlData = new FormData();
+    BlData.append('name', this.state.bname);
+    this.addBucketlist(this.baseURL, BlData);
+    this.setState({ bname: '' });
   }
 
   componentDidMount() {
@@ -69,9 +133,32 @@ class Bucketlist extends Component {
           <li ><a href="#">Logout</a></li>
         </ul>
       </nav>
-      <button type="submit" className="btn btn-xs">
-        <i className="glyphicon glyphicon-trash" /></button>
+      <button className="btn btn-xs" onClick={this.openModal}>
+        <i className="glyphicon glyphicon-plus-sign" /></button>
       <BucketlistTable bucketlists={this.state.bucketlists} />
+      <Modal show={this.state.showModal} onHide={this.closeModal} {...this.props} bsSize="small" aria-labelledby="contained-modal-title-sm" >
+        <Modal.Header closeButton>
+          <Modal.Title>Add Bucketlist</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={this.handleAdd}>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                value={this.state.bname}
+                onChange={this.getName.bind(this, 'bname')}
+              />
+            </div>
+            <button type="submit" className="btn btn-default">Add Bucketlist</button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={this.closeModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </div>
     );
   }
