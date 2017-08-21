c03238fc4c892cbf210a851cc96183df20c1b02f
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-21 14:42:02 +0300

    File to display bucketlist items

---
+++ src/components/bucketlistItems.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import '../styles/css/bootstrap.min.css';
+import '../styles/css/bucketlist.css';
+
+class BLIRow extends Component {
+  render() {
+    return (
+      <li className="list-group-item">
+        {this.props.bucketlist}
+      </li>
+    );
+  }
+}
+
+class BucketlistItems extends Component {
+  render() {
+    const rows = [];
+    const blsi = this.props.items;
+    for (const key in blsi) {
+      if (blsi.hasOwnProperty(key)) {
+        rows.push(<BLIRow id={key} bucketlist={blsi[key]} token={this.props.token} />);
+      }
+    }
+    return (<div>
+      <ul className="list-group  col-md-offset-4 col-md-3">
+        {rows}
+      </ul>
+    </div>
+    );
+  }
+}
+
+export default BucketlistItems;
