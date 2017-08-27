8105f9c0ee2f61eebdf553bdf38f6727b8dac4c6
lornatumuhairwe <lornatumuhairwe@gmail.com>
2017-08-27 08:46:34 +0300

    Logout function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -85,7 +85,7 @@ class LoginForm extends Component {
       );
     }
     return (
-      <Bucketlist token={this.token} />
+      <Bucketlist token={this.token} isLoggedIn={this.state.isLoggedIn} />
     );
   }
 }
--- src/components/bucketlist.jsx
+++ src/components/bucketlist.jsx
@@ -3,11 +3,12 @@ import { Modal, Button } from 'react-bootstrap';
 import '../styles/css/bootstrap.min.css';
 import '../styles/css/bucketlist.css';
 import BucketlistTable from './bucketlistTable';
+import LoginForm from './Login';
 
 class Bucketlist extends Component {
   constructor(props) {
     super(props);
-    this.state = { bucketlists: '', showModal: false, bname: '' };
+    this.state = { bucketlists: '', showModal: false, bname: '', isLoggedIn: this.props.isLoggedIn };
     this.closeModal = this.closeModal.bind(this);
     this.openModal = this.openModal.bind(this);
     this.addBucketlist = this.addBucketlist.bind(this);
@@ -16,6 +17,7 @@ class Bucketlist extends Component {
     this.baseURL = 'https://tofaangapi.herokuapp.com/bucketlists/';
     this.token = this.props.token;
     this.getBucketlists = this.getBucketlists.bind(this);
+    this.LogOut = this.LogOut.bind(this);
   }
 
   getBucketlists(url) {
@@ -38,6 +40,11 @@ class Bucketlist extends Component {
     const newState = {};
     newState[field] = event.target.value;
     this.setState(newState);
+  }
+  LogOut(e) {
+    e.preventDefault();
+    alert(`Log me out ${this.state.isLoggedIn}`);
+    this.setState({ isLoggedIn: false });
   }
   addBucketlist(url, data) {
     const postData = {
@@ -67,43 +74,47 @@ class Bucketlist extends Component {
   }
 
   render() {
-    return (<div>
-      <nav className="navbar navbar-inverse">
-        <ul className="nav navbar-nav">
-          <li><a href="#">TOFAANGA</a></li>
-        </ul>
-        <ul className="nav navbar-nav navbar-right">
-          <li ><a href="#">Logout</a></li>
-        </ul>
-      </nav>
-      <button className="btn btn-xs" onClick={this.openModal}>
-        <i className="glyphicon glyphicon-plus-sign" />Add Bucketlist</button>
-      <BucketlistTable bucketlists={this.state.bucketlists} token={this.props.token} getBucketlists={this.getBucketlists} />
-      <Modal show={this.state.showModal} onHide={this.closeModal} {...this.props} bsSize="small" aria-labelledby="contained-modal-title-sm" >
-        <Modal.Header closeButton>
-          <Modal.Title>Add Bucketlist</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={this.handleAdd}>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                value={this.state.bname}
-                onChange={this.getName.bind(this, 'bname')}
-              />
-            </div>
-            <button type="submit" className="btn btn-default">Add Bucketlist</button>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={this.closeModal}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    </div>
-    );
+    if (this.state.isLoggedIn) {
+      return (<div>
+        <nav className="navbar navbar-inverse">
+          <ul className="nav navbar-nav">
+            <li><a href="">TOFAANGA</a></li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li><a onClick={this.LogOut}>Logout</a></li>
+          </ul>
+        </nav>
+        <button className="btn btn-xs" onClick={this.openModal}>
+          <i className="glyphicon glyphicon-plus-sign" />Add Bucketlist</button>
+        <BucketlistTable bucketlists={this.state.bucketlists} token={this.props.token} getBucketlists={this.getBucketlists} />
+        <Modal show={this.state.showModal} onHide={this.closeModal} {...this.props} bsSize="small" aria-labelledby="contained-modal-title-sm" >
+          <Modal.Header closeButton>
+            <Modal.Title>Add Bucketlist</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={this.handleAdd}>
+              <div className="form-group">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="name"
+                  value={this.state.bname}
+                  onChange={this.getName.bind(this, 'bname')}
+                />
+              </div>
+              <button type="submit" className="btn btn-default">Add Bucketlist</button>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.closeModal}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      );
+    }
+
+    return (<LoginForm />);
   }
 }
 
