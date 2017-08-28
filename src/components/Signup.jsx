7ad7623735ee1bf97464f43bb1b96a9cf49de724
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-08-28 04:38:19 +0300

    Feature sign up

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import '../styles/css/bootstrap.min.css';
 import '../styles/css/style.css';
+import Bucketlist from './bucketlist';
 
 class SignupForm extends Component {
   constructor(props) {
     super(props);
-    this.state = { name: '', email: '', dob: '', pwd: '' };
+    this.state = { name: '', email: '', dob: '', pwd: '', token: '', isLoggedIn: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.baseURL = 'https://tofaangapi.herokuapp.com/auth/register';
   }
   handleSubmit(event) {
+    event.preventDefault();
     const signupFormData = new FormData();
     signupFormData.append('name', this.state.name);
     signupFormData.append('email', this.state.email);
     signupFormData.append('birthdate', this.state.dob);
     signupFormData.append('password', this.state.pwd);
     this.sendRegistration(this.baseURL, signupFormData);
-    event.preventDefault();
     this.setState({ name: '', email: '', dob: '', pwd: '' });
   }
   handleChange(field, event) {
@@ -32,61 +33,54 @@ class SignupForm extends Component {
       body: data,
     };
     fetch(url, postData)
-      .then(response => console.log(response.json().message));
+      .then(response => response.json())
+        .then(resjson => {
+          if (resjson.message==='Registration Sucessful')
+          {
+            this.setState({isLoggedIn: true, token: resjson.auth_token})
+          }
+        }
+  )
   }
   render() {
-    return (
-      <form className="col-md-offset-3 col-md-4" onSubmit={this.handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            value={this.state.name}
-            required
-            onChange={this.handleChange.bind(this, 'name')}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            className="form-control"
-            id="email"
-            value={this.state.email}
-            required
-            onChange={this.handleChange.bind(this, 'email')}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="dob">Date of Birth:</label>
-          <input
-            type="date"
-            className="form-control"
-            id="dob"
-            value={this.state.dob}
-            required
-            onChange={this.handleChange.bind(this, 'dob')}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            type="password"
-            className="form-control"
-            id="pwd"
-            value={this.state.pwd}
-            required
-            onChange={this.handleChange.bind(this, 'pwd')}
-          />
-        </div>
-        <button type="submit" className="btn btn-default">Sign up</button>
-        <div>
-          <a href="/">Back to Home</a>
-        </div>
-      </form>
-    );
+    if (!this.state.isLoggedIn){
+        return (
+            <div className="container">
+              <div className="row">
+                <div className="col-sm-6 col-md-4 col-md-offset-4">
+                  <h1 className="text-center login-title">Sign up start keeping track of your bucketlists </h1>
+                  <div className="account-wall">
+                    <form className="form-signin" onSubmit={this.handleSubmit}>
+                      <input type="text" id="name" className="form-control"
+                             placeholder="Name" value={this.state.name}
+                             required autoFocus onChange={this.handleChange.bind(this, 'name')} />
+                      <input type="text" id="email" className="form-control"
+                             placeholder="Email" value={this.state.email}
+                             required autoFocus onChange={this.handleChange.bind(this, 'email')} />
+                      <input type="date" id="bd" className="form-control"
+                             placeholder="Birth Date" value={this.state.dob}
+                             required autoFocus onChange={this.handleChange.bind(this, 'dob')} />
+                      <input type="password"  className="form-control" id="pwd"
+                             value={this.state.pwd} placeholder="Password" required
+                             onChange={this.handleChange.bind(this, 'pwd')} />
+                      <button className="btn btn-lg btn-primary btn-block" type="submit">
+                        Sign up</button>
+                      <a href="" className="pull-right need-help">Forgot Password? </a>
+                      <span className="clearfix"/>
+                    </form>
+                  </div>
+                  <a href="" className="text-center new-account"
+                     onClick={this.props.displayLoginForm}>Already have an account? Sign in</a>
+                </div>
+              </div>
+            </div>
+        );
+    }
+
+      return (
+          <Bucketlist token={this.token} isLoggedIn={this.state.isLoggedIn} />
+      );
+
   }
 }
 
