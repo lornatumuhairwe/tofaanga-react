446502f9907d36eff0982a1642d33c98801e201f
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-08-28 10:21:04 +0300

    export default on top

--- src/components/Err404.jsx
+++ src/components/Err404.jsx
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-class Err404 extends Component {
-    render(){
-        return (
-            <div>
-                <h1>Error, route not found</h1>
-                <p>Back to <a href="/">Home</a></p>
-            </div>);
 
-    }
+export default class Err404 extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Error, route not found</h1>
+        <p>Back to <a href="/">Home</a></p>
+      </div>);
+  }
 }
 
-export default Err404;
