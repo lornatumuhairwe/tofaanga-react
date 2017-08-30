72886ceffcdfb2c83a892acd6d84f149dd268b2a
Lornah Lynn Tumuhairwe <lornah.tumuhairwe@andela.com>
2017-08-31 02:49:02 +0300

    createdsearch component and functionality

---
+++ src/components/search.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import '../styles/css/bootstrap.min.css';
+import { FormGroup, Button, InputGroup, FormControl } from 'react-bootstrap';
+import { baseUrl } from '../constants';
+
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { search: '', result: [] };
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(field, event) {
+    const newState = {};
+    newState[field] = event.target.value;
+    this.setState(newState);
+  }
+
+  handleSearch() {
+    this.props.getBucketlists(`${baseUrl}bucketlists/?q=${this.state.search}`);
+  }
+
+  render() {
+    return (<div>
+      <form className="col-md-offset-4 col-md-4">
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" onChange={this.handleChange.bind(this, 'search')} />
+            <InputGroup.Button>
+              <Button onClick={this.handleSearch}>
+                <i className="glyphicon glyphicon-search" />
+              </Button>
+            </InputGroup.Button>
+          </InputGroup>
+        </FormGroup>
+      </form>
+    </div>);
+  }
+}
